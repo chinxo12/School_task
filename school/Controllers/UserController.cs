226833af7c52d6a66ae925049e54c10a66b9241d
chinxo12 <111996252+chinxo12@users.noreply.github.com>
2023-04-26 15:27:23 +0700

    Update UserController.cs

--- school/Controllers/UserController.cs
+++ school/Controllers/UserController.cs
@@ -1,5 +1,6 @@
-﻿
+
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using school.Data;
 using school.Models;
 
@@ -31,17 +32,75 @@ namespace school.Controllers
      
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(User user)
+        public IActionResult Create(int classId,User user)
         {
-            if (ModelState.IsValid)
-            {
-              
-                user.CreatedDate = DateTime.Now;
 
-                _context.Users.Add(user);
-                _context.SaveChanges();
-                return RedirectToAction(nameof(Index));
+            if (ModelState.IsValid)
+            { 
+
+                bool check = true;
+                int count = 0;
+                int countF = 0;
+                user.CreatedDate = DateTime.Now;
+                Class classs = _context.Classes.Find(classId);
+                if (classs.ClassId == classId)
+                {
+                    user.Class = classs;
+                }
+                else
+                {
+                    return NotFound();
+                }
+                if (user.Role.RoleId==1)
+                {
+                    return NotFound();
+                }
+                if (user.Role.RoleId == 3)
+                {
+                    // kiểm tra xem số lượng hiện tại của School.
+                    var result = _context.Users
+                            .Join(_context.Classes, u => u.ClassId, c => c.ClassId, (u, c) => new { User = u, Class = c })
+                            .Join(_context.Faculties, uc => uc.Class.FacultyId, f => f.FacultyId, (uc, f) => new { UserClass = uc, Faculty = f })
+                            .GroupBy(fc => fc.Faculty.SchoolId)
+                            .Select(g => new { SchoolId = g.Key, Count = g.Count() })
+                            .ToList();
+
+                    foreach (var item in result)
+                    {
+                        if (item.SchoolId == user.Class.Faculty.SchoolId)
+                        {
+                             count = item.Count;
+                        }   
+                    }
+                    // Kiểm tra số lượng hiện tại của Khoa
+                   var result1 = from u in _context.Users join c in _context.Classes on u.ClassId equals c.ClassId
+                                 group u by c.FacultyId into g
+                                 select new {FacultyId = g.Key,Count = g.Count() };
+                    foreach (var item in result1)
+                    {
+                        if (item.FacultyId == user.Class.FacultyId)
+                        {
+                            countF = item.Count;
+                        }
+                    }
+
+                }
+                if (count > 1000 || countF>1000)
+                {
+                    check = false;
+                }
+                if (check)
+                {
+                    _context.Users.Add(user);
+                    _context.SaveChanges();
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return NotFound();
+                }
             }
+
             return View(user);
         }
 
@@ -72,7 +131,7 @@ namespace school.Controllers
             }
 
             if (ModelState.IsValid)
-            {
+            { 
                 _context.Update(user);
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
